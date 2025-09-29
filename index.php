95048a9454badfa92ccc37847e3bed1de91f770f
Fernand41 <marilucmetchihoungbe@gmail.com>
2025-09-29 23:00:58 +0100

    four-commit

--- index.php
+++ index.php
@@ -7,26 +7,16 @@ $page_css = "index.css";
 require 'header.php';
 ?>
 
-
 <main>
   <section class="hero" id="hero">
-    <!-- Vidéo en arrière-plan -->
-<video autoplay muted loop playsinline preload="auto" class="hero-video" poster="assets/vidéo/poster.jpg">
-    <source src="assets/vidéo/video.mp4" type="video/mp4">
-    <source src="assets/vidéo/video.webm" type="video/webm">
-    <!-- Fallback pour les navigateurs qui ne supportent pas la vidéo -->
-    <img src="assets/image/poster.jpg" alt="Background alternative">
-</video>
-
-    <!-- Contenu par-dessus -->
+    <!-- Contenu principal -->
     <div class="hero-content">
         <h1>Révolutionner les Job's en  <span class="highlight">Informatique</span> grâce à la <span class="highlight">blockchain</span></h1>
         <p>
             Blockchain Job's a pour mission d'aider les jeunes diplômés passionnés par le Web 3 à trouver leur voie professionnelle. Notre plateforme connecte les talents aux entreprises nationales et internationales pour construire ensemble l'avenir de la blockchain.
         </p>
     </div>
-</section>
-   
+  </section>
 </main>
 
 <?php
